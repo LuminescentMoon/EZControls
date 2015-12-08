9ec8e1fff19a30b44c403e2e6e71dfeb2a33a951
Howard Nguyen <Luminess@users.noreply.github.com>
2015-12-08 00:15:43 -0800

    Removed accidental file.

--- test.lua
+++
@@ -1,7 +0,0 @@
-local dkjson = require('lib.dkjson.dkjson')
-
-local function test()
-  print('ayy lmao')
-end
-
-print(dkjson.encode({ testObj = test }, { exception = function() return true end }))
